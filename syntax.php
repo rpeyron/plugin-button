092313a5d0dc7fd54e485ed1e67899ad55a4384e
Remi Peyronnet <remi+github@via.ecp.fr>
2022-12-13 20:00:56 +0100

    Update syntax.php
    Should fix #25  #26 #27

--- syntax.php
+++ syntax.php
@@ -30,6 +30,7 @@ All fields optional, minimal syntax:
  04/08/2020 : Quick hack to add compatibility with hogfather
  07/02/2022 : Added Português do Brasil translation (PR by mac-sousa)
  26/11/2022 : Fixed PHP8.1 warnings
+ 13/12/2022 : Fixed PHP7 with str_contains polyfill
  
  @author ThisNameIsNotAllowed
  17/11/2016 : Added generation of metadata
